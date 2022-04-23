c25fbc67c9ee65c49959166f51955a94dce71bf7
hakyung kim <96482380+hk9713@users.noreply.github.com>
2022-04-24 00:13:52 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,19 +8,23 @@ def main():
         model_cbow = pickle.load(f)
 
     st.title("키워드를 통한 제주도 맛집 추천 시스템")
-
+    
     input_keyword = st.text_input(label="Search Keyword", value="키워드를 입력해주세요")
-
+    
     if st.button("Search"):
         con = st.container()
         con.caption("Result")
+        
         try:
             con.write(f"요청하신 키워드는 '{str(main_keyword)}' 입니다.")
             keywords=model_cbow.wv.most_similar(input_keyword)
             main_keyword=keywords[0][0]
+            flag = True
         except:
             con.info("유사한 키워드를 가지고 있는 식당이 없습니다. 다른 키워드를 입력해주세요")
-
+            flag = False
+         
+    if flag == True:
         weighted_series=pd.Series(df['token'].apply(lambda x:1))
         for keyword, weight in keywords:
             count = pd.Series(df['token'].apply(lambda x:x.count(keyword)))
@@ -32,6 +36,7 @@ def main():
             index=index[:5]
 
         con.table(df['상호명'][index])
+        
         
 if __name__ == '__main__' :
     main()
