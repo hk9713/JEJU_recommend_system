2591869b9188a2d1bffd86850acb035993844a7c
hakyung kim <96482380+hk9713@users.noreply.github.com>
2022-04-24 01:06:56 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,9 @@ def main():
                 keywords=model_cbow.most_similar(input_keyword)
                 main_keyword=keywords[0][0]
                 flag = True
-            except:
+            except e1:
                 con.info("유사한 키워드를 가지고 있는 식당이 없습니다. 다른 키워드를 입력해주세요")
+                con.write(e1)
                 flag = False
          
     if flag:
